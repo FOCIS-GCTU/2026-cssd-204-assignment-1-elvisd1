a30698b48bdbd3a278b7618ed06e2c1b71dc391e
elvisd1 <elvisdeku243@gmail.com>
2026-05-30 22:16:49 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Print out my initials in stylized large letters
+# Assignment Number: 1 
+# 
+# Name: ELVIS ERIC DEKU
+# STUDENT ID: 2425402982
+# Email: 2425402982@live.gctu.edu.gh
+# Grader: AUGUSTUS BUCKMAN
+# 
+# On my honor, ELVIS ERIC DEKU, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    # Prints initials in 12x10 blocks with 4-asterisk periods and spacing
+    print()
+    print("...EED")
+    print()
+    print("...EEEEEEEEEEEE.........EEEEEEEEEEEE.........DDDDDDDDDD....")
+    print("...EEEEEEEEEEEE.........EEEEEEEEEEEE.........DDDDDDDDDDDD..")
+    print("...EE...................EE...................DD.......DDD..")
+    print("...EE...................EE...................DD........DD..")
+    print("...EEEEEEEE.............EEEEEEEE.............DD........DD..")
+    print("...EEEEEEEE.............EEEEEEEE.............DD........DD..")
+    print("...EE...................EE...................DD........DD..")
+    print("...EE...................EE...................DD.......DDD..")
+    print("...EEEEEEEEEEEE...**....EEEEEEEEEEEE...**....DDDDDDDDDDDD..**")
+    print("...EEEEEEEEEEEE...**....EEEEEEEEEEEE...**....DDDDDDDDDD....**")
+    print()
 
 
 main()
